3df81a9d89b1046be4c998343459562ef0395630
Tim Weiland <tim@weiland-lahnstein.de>
2024-02-20 17:29:22 +0100

    Removing ambiguities in concatenated and sum linfuncops

--- src/linearfunctionoperators/arithmetic/concatenate.jl
+++ src/linearfunctionoperators/arithmetic/concatenate.jl
@@ -1,13 +1,26 @@
 export AbstractConcatenatedLinearFunctionOperator, ConcatenatedLinearFunctionOperator
+using AbstractGPs: AbstractGP
 
 abstract type AbstractConcatenatedLinearFunctionOperator <: AbstractLinearFunctionOperator end
 linfuncops(op::AbstractConcatenatedLinearFunctionOperator) = op.linfuncops
-function (op::AbstractConcatenatedLinearFunctionOperator)(x, args...)
+function (op::AbstractConcatenatedLinearFunctionOperator)(
+    x::T,
+    args...;
+    kwargs...,
+) where {T}
     res = x
     for linfuncop in linfuncops(op)
-        res = linfuncop(res, args...)
+        res = linfuncop(res, args...; kwargs...)
     end
     return res
+end
+_fallback(op::AbstractConcatenatedLinearFunctionOperator, x, args...; kwargs...) = invoke(op, Tuple{Any}, x, args...; kwargs...)
+(op::AbstractConcatenatedLinearFunctionOperator)(x::EvaluationPVCrosscov) = _fallback(op, x)
+(op::AbstractConcatenatedLinearFunctionOperator)(x::StackedPVCrosscov) = _fallback(op, x)
+(op::AbstractConcatenatedLinearFunctionOperator)(x::ZeroMean{T}, args...) where {T} = _fallback(op, x, args...)
+
+function (ℒ::AbstractConcatenatedLinearFunctionOperator)(f::AbstractGP; noise::TΣy = 0) where {TΣy}
+    return LinfctlTransformedGP(f, ℒ, ℒ(f.mean), ℒ(ℒ(f.kernel)), noise)
 end
 
 function Base.show(io::IO, op::AbstractConcatenatedLinearFunctionOperator)
--- src/linearfunctionoperators/arithmetic/sum.jl
+++ src/linearfunctionoperators/arithmetic/sum.jl
@@ -9,3 +9,11 @@ end
 function Base.show(io::IO, op::SumLinearFunctionOperator)
     return print(io, join(["($(string(summand)))" for summand in summands(op)], " + "))
 end
+
+_fallback(op::SumLinearFunctionOperator, x, args...; kwargs...) = invoke(op, Tuple{Any}, x, args...; kwargs...)
+(op::SumLinearFunctionOperator)(x::EvaluationPVCrosscov) = _fallback(op, x)
+(op::SumLinearFunctionOperator)(x::StackedPVCrosscov) = _fallback(op, x)
+(op::SumLinearFunctionOperator)(x::ZeroMean{T}, args...) where {T} = _fallback(op, x, args...)
+function (ℒ::SumLinearFunctionOperator)(f::AbstractGP; noise::TΣy = 0) where {TΣy}
+    return LinfctlTransformedGP(f, ℒ, ℒ(f.mean), ℒ(ℒ(f.kernel)), noise)
+end
