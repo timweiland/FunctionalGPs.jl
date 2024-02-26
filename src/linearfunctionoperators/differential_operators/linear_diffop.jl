67e24a47d7a4135ee0d8698c0a33d463ed9dde71
Tim Weiland <tim@weiland-lahnstein.de>
2024-02-26 17:26:45 +0100

    Revamp `LinearDifferentialOperator`

--- src/linearfunctionoperators/differential_operators/linear_diffop.jl
+++ src/linearfunctionoperators/differential_operators/linear_diffop.jl
@@ -1,29 +1,35 @@
-export LinearDifferentialOperator
+export LinearDifferentialOperator,
+    PartialDerivativeCoefficients,
+    MultiIndex,
+    MultiDict,
+    PartialDerivativeCoefficients,
+    MaybeScaledPartialDerivative
+
+const MultiIndex{M} = NTuple{M,Int}
+const MultiDict{M,T} = Dict{MultiIndex{M},T} where {T<:Real}
+const PartialDerivativeCoefficients{M,T} = Dict{Int,D} where {M,T<:Real,D<:MultiDict{M,T}}
+const MaybeScaledPartialDerivative{N,M} = Union{
+    ConstantScaledLinearFunctionOperator{PartialDerivative{N,M}},
+    PartialDerivative{N,M},
+}
 
 struct LinearDifferentialOperator{K,N,M} <: AbstractSumLinearFunctionOperator{K}
-    idx_dict::Dict{K,AbstractVector{NTuple{M,Integer}}}
-    summands::Vector{PartialDerivative}
+    idx_dict::PartialDerivativeCoefficients{M}
+    summands::NTuple{K,MaybeScaledPartialDerivative{N,M}}
 
-    function LinearDifferentialOperator{N,M}(
-        idx_dict::Dict{K,Vector{T}},
-    ) where {N,M,K<:Integer,T<:NTuple{M,Integer}}
+    function LinearDifferentialOperator{N}(
+        idx_dict::PartialDerivativeCoefficients{M},
+    ) where {N,M}
         if !all(1 <= key <= N for key in keys(idx_dict))
             throw(DomainError("Keys of idx_dict must be between 1 and $N"))
         end
-        summands = []
-        for (output_idx, multi_idx_list) in idx_dict
-            for multi_idx in multi_idx_list
-                push!(summands, PartialDerivative{N,M}(output_idx, multi_idx))
+        summands = Vector{MaybeScaledPartialDerivative{N,M}}()
+        for (output_idx, multi_idx_dict) in idx_dict
+            for (multi_idx, coeff) in multi_idx_dict
+                push!(summands, coeff * PartialDerivative{N,M}(output_idx, multi_idx))
             end
         end
-        return new{K,N,M}(idx_dict, summands)
+        K = length(summands)
+        return new{K,N,M}(idx_dict, tuple(summands...))
     end
-end
-
-function LinearDifferentialOperator{N}(idx_dict::Dict{K,Vector{T}}) where {N,M,K<:Integer,T<:NTuple{M,Integer}}
-    return LinearDifferentialOperator{N,M}(idx_dict)
-end
-
-function LinearDifferentialOperator(idx_dict::Dict{K,Vector{T}}) where {M,K<:Integer,T<:NTuple{M,Integer}}
-    return LinearDifferentialOperator{1,M}(idx_dict)
 end
