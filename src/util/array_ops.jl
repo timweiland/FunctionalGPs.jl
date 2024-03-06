dabfb45be4ab441c4d0843a1db862c2cc7411642
Tim Weiland <tim@weiland-lahnstein.de>
2024-03-06 14:20:38 +0100

    Add `reshape_product_broadcast` utility

--- src/util/array_ops.jl
+++ src/util/array_ops.jl
@@ -1,4 +1,4 @@
-export moveaxis
+export moveaxis, reshape_product_broadcast
 
 function moveaxis(A::AbstractArray, source::Int, dest::Int)
     if source == dest
@@ -7,4 +7,26 @@ function moveaxis(A::AbstractArray, source::Int, dest::Int)
     perm = collect(1:ndims(A))
     insert!(perm, dest, splice!(perm, source))
     return permutedims(A, perm)
+end
+
+"""
+    reshape_product_broadcast(A::AbstractArray, B::AbstractArray)
+
+    Reshape `A` and `B` by adding singleton dimensions to enable broadcast 
+    operations on the Cartesian product of the two arrays.
+
+    # Example
+    ```julia
+    A = rand(4, 5)
+    B = rand(2, 3)
+    A, B = reshape_product_broadcast(A, B)
+    C = A .+ B
+    @assert size(C) == (4, 5, 2, 3)
+    ```
+"""
+function reshape_product_broadcast(A::AbstractArray, B::AbstractArray)
+    ndims_A = ndims(A)
+    A = reshape(A, size(A)..., ones(Int, ndims(B))...)
+    B = reshape(B, ones(Int, ndims_A)..., size(B)...)
+    return A, B
 end
--- test/util/test_array_ops.jl
+++ test/util/test_array_ops.jl
@@ -17,4 +17,15 @@ import Random: seed!
         @test moveaxis(A, 3, 2) == permutedims(A, (1, 3, 2))
         @test moveaxis(A, 3, 3) == A
     end
+
+    @testset "reshape product broadcast" begin
+        A_old = rand(4, 5)
+        B_old = rand(2, 3)
+        A, B = reshape_product_broadcast(A_old, B_old)
+        C = A .+ B
+        @test size(C) == (4, 5, 2, 3)
+        for i in 1:4, j in 1:5, k in 1:2, l in 1:3
+            @test C[i, j, k, l] == A_old[i, j] + B_old[k, l]
+        end
+    end
 end
