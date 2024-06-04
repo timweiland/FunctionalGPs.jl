a88b938b599af3ccda861821eba8ae3281257948
lucaf <fueger00@gmail.com>
2024-06-04 09:49:36 +0200

    intrinsic and extrinsic matern impl + small tests

---
+++ test/kernels/test_spherical_matern.jl
@@ -0,0 +1,27 @@
+include("../../src/kernels/SphericalMatern.jl")
+
+using .MaternKernelModule
+
+# Create an intrinsic MaternKernel instance
+# just using a dummy value for the variance
+intrinsic_matern_kernel = IntrinsicMatern(MaternKernel(1.5, 2.0, 1.0))
+
+# Create an extrinsic MaternKernel instance
+# just using a dummy value for the variance
+extrinsic_matern_kernel = ExtrinsicMatern(MaternKernel(1.5, 2.0, 1.0))
+
+# Define angles for evaluation
+θ1, φ1 = π / 4, π / 4
+θ2, φ2 = π / 3, π / 3
+
+# Evaluate intrinsic Matern kernel
+result_intrinsic = evaluate(intrinsic_matern_kernel,θ1, φ1, θ2, φ2)
+
+# Evaluate extrinsic Matern kernel
+result_extrinsic = evaluate(extrinsic_matern_kernel, θ1, φ1, θ2, φ2)
+
+println("Intrinsic Matern kernel evaluation result: $result_intrinsic")
+println("Extrinsic Matern kernel evaluation result: $result_extrinsic")
+
+
+# TODO: Implement a more sophisticated test for both the intrinsic and extrinsic Matern kernel
