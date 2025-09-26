e21da38ac5ab8e95afd3ea56987b3e39312cd6c9
Tim Weiland <tim@weiland-lahnstein.de>
2025-09-26 16:06:57 +0300

    Rework EvaluationPVCrosscov API

--- src/pv_crosscovs/evaluation.jl
+++ src/pv_crosscovs/evaluation.jl
@@ -2,32 +2,30 @@ using KernelFunctions: Kernel
 
 export EvaluationPVCrosscov
 
-struct EvaluationPVCrosscov <: ProcessVectorCrossCovariance
-    k::Kernel
-    X::AbstractVector
-    eval_arg::Integer
-
-    function EvaluationPVCrosscov(k::Kernel, X::AbstractVector, eval_arg::Integer)
-        @assert eval_arg ∈ [1, 2]
-        return new(k, X, eval_arg)
-    end
+struct EvaluationPVCrosscov{EvalArg, TK <: Kernel, TL} <: ProcessVectorCrossCovariance
+    eval_arg::Int
+    k::TK
+    linfunc::TL
 end
 
-randvar_batch_size(pv::EvaluationPVCrosscov) = size(pv.X)
-randvar_arg(pv::EvaluationPVCrosscov) = pv.eval_arg
-
-function kernelmatrix(pv::EvaluationPVCrosscov, X::AbstractVector)
-    if pv.eval_arg == 1
-        return kernelmatrix(pv.k, pv.X, X)
-    else
-        return kernelmatrix(pv.k, X, pv.X)
-    end
+function EvaluationPVCrosscov(k::Kernel, linfunc, eval_arg::Integer)
+    arg = Int(eval_arg)
+    @assert arg ∈ (1, 2)
+    return EvaluationPVCrosscov{arg, typeof(k), typeof(linfunc)}(arg, k, linfunc)
 end
+
+randvar_batch_size(pv::EvaluationPVCrosscov) = size(pv.linfunc.X)
+randvar_arg(::EvaluationPVCrosscov{EvalArg}) where {EvalArg} = EvalArg
+
+kernelmatrix(pv::EvaluationPVCrosscov{1}, X::AbstractVector) =
+    kernelmatrix(pv.k, pv.linfunc.X, X)
+kernelmatrix(pv::EvaluationPVCrosscov{2}, X::AbstractVector) =
+    kernelmatrix(pv.k, X, pv.linfunc.X)
 
 function Base.isequal(pv1::EvaluationPVCrosscov, pv2::EvaluationPVCrosscov)
-    return pv1.k == pv2.k && pv1.X == pv2.X && pv1.eval_arg == pv2.eval_arg
+    return pv1.k == pv2.k && pv1.linfunc == pv2.linfunc && pv1.eval_arg == pv2.eval_arg
 end
 
 function Base.isapprox(pv1::EvaluationPVCrosscov, pv2::EvaluationPVCrosscov)
-    return pv1.k ≈ pv2.k && pv1.X ≈ pv2.X && pv1.eval_arg == pv2.eval_arg
+    return pv1.k ≈ pv2.k && pv1.linfunc.X ≈ pv2.linfunc.X && pv1.eval_arg == pv2.eval_arg
 end
